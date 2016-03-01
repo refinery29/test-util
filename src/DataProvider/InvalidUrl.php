3c5f7d06c0dbe9e394a6707fec834ae8745cf4ea
Andreas Möller <am@localheinz.com>
2016-03-01 12:23:44 +0100

    Enhancement: Implement InvalidUrl data provider

---
+++ src/DataProvider/InvalidUrl.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * Copyright (c) 2016 Refinery29, Inc.
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace Refinery29\Test\Util\DataProvider;
+
+use Generator;
+use Refinery29\Test\Util\Faker\GeneratorTrait;
+use stdClass;
+
+class InvalidUrl implements DataProviderInterface
+{
+    use GeneratorTrait;
+
+    /**
+     * @return array|Generator
+     */
+    public function data()
+    {
+        $faker = $this->getFaker();
+
+        $values = [
+            null,
+            $faker->boolean(),
+            $faker->word,
+            $faker->words,
+            $faker->randomNumber(),
+            $faker->randomFloat(),
+            new stdClass(),
+        ];
+
+        foreach ($values as $value) {
+            yield [
+                $value,
+            ];
+        }
+    }
+}
---
+++ test/DataProvider/InvalidUrlTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * Copyright (c) 2016 Refinery29, Inc.
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace Refinery29\Test\Util\Test\DataProvider;
+
+use Assert\Assertion;
+use InvalidArgumentException;
+use Refinery29\Test\Util\DataProvider\DataProviderInterface;
+use Refinery29\Test\Util\DataProvider\InvalidUrl;
+
+class InvalidUrlTest extends \PHPUnit_Framework_TestCase
+{
+    public function testImplementsDataProviderInterface()
+    {
+        $dataProvider = new InvalidUrl();
+
+        $this->assertInstanceOf(DataProviderInterface::class, $dataProvider);
+    }
+
+    /**
+     * @dataProvider Refinery29\Test\Util\DataProvider\InvalidUrl::data()
+     *
+     * @param mixed $value
+     */
+    public function testIsNotAUrl($value)
+    {
+        $this->setExpectedException(InvalidArgumentException::class);
+
+        Assertion::url($value);
+    }
+}
