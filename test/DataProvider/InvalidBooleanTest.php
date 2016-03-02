e3822056528a0f422468db41302d1aa54dac23c8
Ante Lucic <ante.lucic450@gmail.com>
2016-03-02 16:57:33 -0500

    invalid boolean tests

---
+++ test/DataProvider/InvalidBooleanTest.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * Copyright (c) 2016 Refinery29, Inc.
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace Refinery29\Test\Util\Test\DataProvider;
+
+use Refinery29\Test\Util\DataProvider\DataProviderInterface;
+use Refinery29\Test\Util\DataProvider\InvalidBoolean;
+
+class InvalidBooleanTest extends \PHPUnit_Framework_TestCase
+{
+    public function testImplementsDataProviderInterface()
+    {
+        $dataProvider = new InvalidBoolean();
+
+        $this->assertInstanceOf(DataProviderInterface::class, $dataProvider);
+    }
+
+    /**
+     * @dataProvider Refinery29\Test\Util\DataProvider\InvalidBoolean::data()
+     *
+     * @param mixed $value
+     */
+    public function testIsNotAString($value)
+    {
+        $this->assertFalse(is_bool($value));
+    }
+}
